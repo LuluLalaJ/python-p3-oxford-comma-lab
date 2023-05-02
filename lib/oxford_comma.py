15213979970de0f381ea2e3ae27e56bb11214cc8
Chen <chen.jiang.1206@gmail.com>
2023-05-02 12:36:55 -0400

    done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,19 @@
+# def oxford_comma(items):
+#     if len(items) == 0:
+#         return "List can't be empty"
+#     elif len(items) == 1:
+#         return(items[0])
+#     elif len(items) == 2:
+#         return(f'{items[0]} and {items[1]}')
+#     else:
+#         before_and = items[:-1]
+#         return ", ".join(before_and) + f', and {items[-1]}'
+
 def oxford_comma(items):
-    return None
+    if len(items) > 1:
+        items[-1] = "and " + items[-1]
+
+    if len(items) > 2:
+        return ', '.join(items)
+    else:
+        return ' '.join(items)
